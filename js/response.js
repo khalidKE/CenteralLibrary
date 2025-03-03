35402d4fc4776cb6b3b78bf4c92732426a3df103
Mustafa <mostafaashraf12367@gmail.com>
2025-03-03 16:52:17 +0200

    complete responsive

--- js/response.js
+++ js/response.js
@@ -1,7 +1,7 @@
 let Nav = document.getElementById('navbar');
 let headers = document.getElementsByTagName('header')[0];
 window.onscroll = function () {
-    if ( window.scrollY > 50 ) {
+    if ( window.scrollY > 10 ) {
         headers.style.display = 'none';
     } else {
         headers.style.display = 'block';
